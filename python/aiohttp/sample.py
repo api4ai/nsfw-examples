1c1fd84b2e50121a1840c7e0f194886b5208be7d
Jahangir Shah <shah@api4.ai>
2022-04-16 02:34:08 +0300

    Add sample.py with example of usage.

---
+++ python/aiohttp/sample.py
@@ -0,0 +1,64 @@
+"""Example of using API4AI NSFW image classification."""
+import asyncio
+import sys
+
+import aiohttp
+
+
+# Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
+# For more details visit:
+#   https://api4.ai
+#
+# Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
+# For more details visit:
+#   https://rapidapi.com/api4ai-api4ai-default/api/nsfw3/details
+MODE = 'demo'
+
+
+# Your RapidAPI key. You need to fill this variable by proper value if you want
+# to try api4ai via RapidAPI marketplace.
+RAPIDAPI_KEY = ''
+
+
+OPTIONS = {
+    'demo': {
+        'url': 'https://demo.api4ai.cloud/nsfw/v1/results',
+        'headers': {'A4A-CLIENT-APP-ID': 'sample'}
+    },
+    'rapidapi': {
+        'url': 'https://nsfw3.p.rapidapi.com/v1/results',
+        'headers': {'X-RapidAPI-Key': RAPIDAPI_KEY}
+    }
+}
+
+
+async def main():
+    """Entry point."""
+    image = sys.argv[1] if len(sys.argv) > 1 else 'https://storage.googleapis.com/api4ai-static/samples/nsfw-1.jpg'  # noqa
+
+    # response = None
+    async with aiohttp.ClientSession() as session:
+        if '://' in image:
+            # Data from image URL.
+            data = {'url': image}
+        else:
+            # Data from local image file.
+            data = {'image': open(image, 'rb')}
+        # Make request.
+        async with session.post(OPTIONS[MODE]['url'],
+                                data=data,  # noqa
+                                headers=OPTIONS[MODE]['headers']) as response:
+            resp_json = await response.json()
+            resp_text = await response.text()
+
+        # Print raw response.
+        print(f'💬 Raw response:\n{resp_text}\n')
+
+        # Parse response and probabilities.
+        probs = resp_json['results'][0]['entities'][0]['classes']
+        print(f'💬 Probabilities:\n{probs}')
+
+
+if __name__ == '__main__':
+    # Parse args.
+    asyncio.run(main())
