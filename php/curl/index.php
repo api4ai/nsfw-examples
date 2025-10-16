5b73a665bd7f7d84e029d2a5266c0d4cfc59b16d
Vladislav Gubarev <gubarev@api4.ai>
2025-10-16 18:26:11 +0300

    Updated PHP sample: Added "normal" mode

--- php/curl/index.php
+++ php/curl/index.php
@@ -3,14 +3,17 @@
 <?php
 // Example of using API4AI NSFW image classification.
 
-// Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
-// For more details visit:
-//   https://api4.ai
-
+// Use 'demo' mode just to try api4ai for free. ⚠️ Free demo is rate limited and must not be used in real projects.
+//
+// Use 'normal' mode if you have an API Key from the API4AI Developer Portal. This is the method that users should normally prefer.
+//
 // Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
 // For more details visit:
 //   https://rapidapi.com/api4ai-api4ai-default/api/nsfw3/details
 $MODE = 'demo';
+
+// Your API4AI key. Fill this variable with the proper value if you have one.
+$API4AI_KEY = null;
 
 // Your RapidAPI key. Fill this variable with the proper value if you want
 // to try api4ai via RapidAPI marketplace.
@@ -19,7 +22,11 @@ $RAPIDAPI_KEY = null;
 $OPTIONS = [
     'demo' => [
         'url' => 'https://demo.api4ai.cloud/nsfw/v1/results',
-        'headers' => ['A4A-CLIENT-APP-ID: sample']
+        'headers' => []
+    ],
+    'normal' => [
+        'url' => 'https://api4ai.cloud/nsfw/v1/results',
+        'headers' => ["X-API-KEY: {$API4AI_KEY}"]
     ],
     'rapidapi' => [
         'url' => 'https://nsfw3.p.rapidapi.com/v1/results',
